152f7f3965c5001603d476ce9cdbf6ef4c70abec
heo960617-ops <heo960617@gmail.com>
2026-09-17 20:10:17 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
+const player = document.getElementById("player");
 
+let x = 50;
+let y = 75;
+
+const speed = 0.5;
+
+document.addEventListener("keydown", function(event) {
+
+    if (event.key === "ArrowUp" || event.key === "w") {
+        y -= speed;
+    }
+
+    if (event.key === "ArrowDown" || event.key === "s") {
+        y += speed;
+    }
+
+    if (event.key === "ArrowLeft" || event.key === "a") {
+        x -= speed;
+    }
+
+    if (event.key === "ArrowRight" || event.key === "d") {
+        x += speed;
+    }
+
+    player.style.left = x + "%";
+    player.style.top = y + "%";
+});
